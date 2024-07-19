13073f206eb5d74d318c1e2cb0ca5890bfea16f1
adithkrishnaor <adithkrishnaor@gmail.com>
2024-07-19 14:27:24 +0530

    Splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/Screens/splashscreen.dart';
 
 void main (){
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        scaffoldBackgroundColor: Color.fromARGB(244, 0, 255, 200)
+        scaffoldBackgroundColor: Color.fromARGB(255, 0, 255, 173)
       ),
       home: SplashScreen(),
     );
