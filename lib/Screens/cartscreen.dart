1086c5430cc41621e2b7b193b4300c5732867883
adithkrishnaor <adithkrishnaor@gmail.com>
2024-07-27 20:18:14 +0530

    Cart screen updated with product

--- lib/Screens/cartscreen.dart
+++ lib/Screens/cartscreen.dart
@@ -18,26 +18,34 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const SafeArea(
+      appBar: AppBar(
+        title: Text('My Cart'),
+        ),
+        body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            SizedBox(
-              height: 20,
+            Text(
+              'Your Cart',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            Center(
-              child: Text(
-                'My Cart',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+            SizedBox(height: 16),
+            ListTile(
+              leading: Image.asset('assets/images/laptop1.jpeg'),
+              title: Text('MacBook'),
+              subtitle: Text('Price: ₹1,07,990'),
+              trailing: Text('Quantity: 1'),
             ),
-
-            
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Add payment logic here
+              },
+              child: Text('Checkout'),
+            ),
           ],
         ),
-      ),
+        ),
       bottomNavigationBar: BottomNavigationBarWidget(
         currentIndex: _currentIndex,
         onTap: _onTap,
