d40a1a554deed0446a96fba91abd24085a744150
adithkrishnaor <adithkrishnaor@gmail.com>
2024-07-27 20:11:12 +0530

    elec details added

--- lib/categories/electronics_screen.dart
+++ lib/categories/electronics_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/product_details/elect_details.dart';
 
 class Electronics_Screen extends StatelessWidget {
   const Electronics_Screen({super.key});
@@ -13,14 +14,44 @@ class Electronics_Screen extends StatelessWidget {
       ),
       body: GridView.count(crossAxisCount: 2,
       children: [
+        InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Elect_Details()),
+              );
+          },
+          child: Card(
+            child: Column(
+              children: [
+                Image.asset('assets/images/laptop1.jpeg',
+                height: 130,
+                ),
+                Text('MacBook'),
+                Text('₹1,07,990'),
+                ],
+            )
+          ),
+        ),
         Card(
           child: Column(
             children: [
-              Image.asset('assets/images/laptop1.jpeg',
+              Image.network('https://www.maplestore.in/cdn/shop/files/iPhone_15_Blue_PDP_Image_Position-1__en-IN_9d151e10-d809-499c-95cd-5c7cb7ea9dbe.jpg?v=1701822313&width=1445',
               height: 130,
               ),
-              Text('Phone'),
-              Text('Rs. 10000'),
+              Text('iPhone'),
+              Text('₹69,399'),
+              ],
+          )
+        ),
+        Card(
+          child: Column(
+            children: [
+              Image.network('https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1709103161/Croma%20Assets/Communication/Mobiles/Images/275155_z2t6g2.png?tr=w-600',
+              height: 130,
+              ),
+              Text('S23 Ultra'),
+              Text('₹104999'),
               ],
           )
         ),
@@ -30,8 +61,8 @@ class Electronics_Screen extends StatelessWidget {
               Image.asset('assets/images/laptop1.jpeg',
               height: 130,
               ),
-              Text('Phone'),
-              Text('Rs. 10000'),
+              Text('MacBook'),
+              Text('₹1,07,990'),
               ],
           )
         ),
@@ -41,41 +72,19 @@ class Electronics_Screen extends StatelessWidget {
               Image.asset('assets/images/laptop1.jpeg',
               height: 130,
               ),
-              Text('Phone'),
-              Text('Rs. 10000'),
+              Text('MacBook'),
+              Text('₹1,07,990'),
               ],
           )
         ),
         Card(
           child: Column(
             children: [
-              Image.asset('assets/images/laptop1.jpeg',
+              Image.network('https://www.maplestore.in/cdn/shop/files/iPhone_15_Blue_PDP_Image_Position-1__en-IN_9d151e10-d809-499c-95cd-5c7cb7ea9dbe.jpg?v=1701822313&width=1445',
               height: 130,
               ),
-              Text('Phone'),
-              Text('Rs. 10000'),
-              ],
-          )
-        ),
-        Card(
-          child: Column(
-            children: [
-              Image.asset('assets/images/laptop1.jpeg',
-              height: 130,
-              ),
-              Text('Phone'),
-              Text('Rs. 10000'),
-              ],
-          )
-        ),
-        Card(
-          child: Column(
-            children: [
-              Image.asset('assets/images/laptop1.jpeg',
-              height: 130,
-              ),
-              Text('Phone'),
-              Text('Rs. 10000'),
+              Text('iPhone'),
+              Text('₹69,399.00'),
               ],
           )
         )
---
+++ lib/product_details/elect_details.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/Screens/cartscreen.dart';
+
+class Elect_Details extends StatelessWidget {
+  const Elect_Details({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Laptop'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image.asset('assets/images/laptop1.jpeg'),
+            const SizedBox(height: 16),
+            Text(
+              'MacBook',
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Apple MacBook Air Apple M3 - (8 GB/256 GB SSD/macOS Sonoma) MRXQ3HN/A  (13 Inch, Silver, 1.24 Kg)',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              '''Highlights
+----------------
+  - Stylish and Portable Thin and Light Laptop
+  13 Inch Liquid Retina display, LED-backlit Display with IPS Technology,
+  - Native Resolution at 224 pixels per inch, 500 nits Brightness
+  - Light Laptop without Optical Disk Drive''',
+              style: TextStyle(
+                fontSize: 16,
+                color: const Color.fromARGB(255, 56, 56, 56),
+              ),
+            ),
+
+            Expanded(child: Container()), // Add this line
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Price: ₹1,07,990',
+                  style: TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(100, 40),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CartScreen()),
+                    );
+                  },
+                  child: const Text('Buy Now'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
